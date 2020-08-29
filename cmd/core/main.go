16e53f83f1d381a04337b196d22dff4688ed0cc2
Aliou Amar <amarraliou97@gmail.com>
2020-08-29 11:17:18 +0100

    Setup main

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/amaraliou/trackr-v2/internal/server"
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Error getting env %v\n", err)
+	}
+
+	server, err := server.NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.Logger.Infof("Starting server")
+	server.ListenAndServe()
 }
